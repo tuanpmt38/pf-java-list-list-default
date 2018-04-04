c5821b02c04fae009062bdf4adcec7817d52bc54
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-04 11:54:42 +0700

    pf-java-list list default

--- MyListTest.java
+++ MyListTest.java
@@ -2,29 +2,20 @@ import java.util.*;
 
 public class MyListTest {
     public static void main(String[] args) {
-        Collection<String> collection = new ArrayList<>();
-        collection.add("New	York");
-        collection.add("Atlanta");
-        collection.add("Dallas");
-        collection.add("Madison");
-        Iterator<String> iterator = collection.iterator();
-        while (iterator.hasNext()) {
-            System.out.print(iterator.next().toUpperCase() + "	");
-        }
-        List<String> arrayList = new ArrayList<>();
-        arrayList.add("New	York");
-        arrayList.add("Atlanta");
-        arrayList.add("Dallas");
-        arrayList.add("Madison");
-        System.out.println(arrayList);
-        List<String> linkedList = new LinkedList<>();
-        linkedList.add("New	York");
-        linkedList.add("Atlanta");
-        linkedList.add("Dallas");
-        linkedList.add("Madison");
-        ListIterator<String> listIterator = linkedList.listIterator();
-        while (listIterator.hasNext()) {
-            System.out.print(listIterator.next() + "	");
-        }
+        MyList<Integer> listInteger = new MyList<>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(3);
+        listInteger.add(4);
+
+        System.out.println("element 4: "+listInteger.get(4));
+        System.out.println("element 1: "+listInteger.get(1));
+        System.out.println("element 2: "+listInteger.get(2));
+
+        listInteger.get(6);
+        System.out.println("element 6: "+listInteger.get(6));
+        listInteger.get(-1);
+        System.out.println("element -1: " + listInteger.get(-1));
     }
 }
